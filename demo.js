4f126bde1610b00466c525f09436a41221eceb1f
unknown <bearic74@gmail.comgit config --global user.name 'Eric Davisgit config --global user.email bearic74@gmail.com>
2019-07-08 15:12:12 -0600

    Started on Guess The Number

---
+++ demo.js
@@ -0,0 +1,12 @@
+
+let yourName = "Eric";
+// console.log(yourName);
+
+function sayYourName (name) {
+    console.log("Hello " + name +", how are you doing?");
+    console.log("Hello " + yourName +", how are you doing?");
+}
+
+// console.log(name);
+sayYourName("Bob");
+sayYourName("Sally");
